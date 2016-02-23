2662470f34eb0b524d42b9b29fa24a9361e19620
root <root@ip-172-31-1-246.us-west-2.compute.internal>
2016-02-23 09:05:50 -0500

    Listen on 3128 only, added nc for debugging purposes

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
-FROM tianon/centos:6.5
+FROM centos:centos7
 MAINTAINER akram.benaissi@gmail.com
-
+#RUN yum update -y
 RUN yum install -y squid
 ADD squid.conf /etc/squid/squid.conf
 ADD run.sh /run.sh
 
 # Make cache dirs 
 RUN squid -z -F
-
+RUN yum install -y nc
 EXPOSE 3128
+EXPOSE 80
 CMD /run.sh
--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-cd $(dirname $0)
-docker build -t akram/docker-squid .
-docker tag -f $(docker build . |grep built | cut -f3 -d\ ) docker-squid:latest
+if [ $# -ne 1 ]; then
+  echo "$0 script requires parameter tag-name"
+  exit 1
+fi
 
+cd $(dirname $0)
+docker build -t docker.io/akrambenaissi/docker-squid .
+docker tag -f $(docker build . |grep built | cut -f3 -d\ ) docker.io/akrambenaissi/docker-squid:latest
+docker tag -f $(docker build . |grep built | cut -f3 -d\ ) docker.io/akrambenaissi/docker-squid:$1
